0a93c0481dfd4ec8e4776b7f99f7489cf97b0343
Home_desktop <creator83@yandex.ru>
2016-05-29 20:07:28 +0300

    29_05

--- cpp/prj/use_nrf24l01.cpp
+++ cpp/prj/use_nrf24l01.cpp
@@ -4,12 +4,12 @@
 #include "tact.h"
 #include "Spi.h"
 #include "nrf24l01.h"
-#include "uart.h"
+#include "pcd8544.h"
 
 
 	tact frq;
-	uart uart1 (uart::baud9600);
 	nrf24l01 radio;
+	pcd8544 lcd;
 	const char led = 0;
 	Gpio B (Gpio::B);
 
@@ -24,18 +24,18 @@ void EXTI0_1_IRQHandler()
 	EXTI->PR |= 1 << 1;
 	uint8_t status = radio.get_status ();
 	radio.w_reg (STATUS, status);
-	uart1.transmit("====");
-	uart1.transmit(status);
-	uart1.transmit(radio.receive_byte());
+
 	B.setPin (led);
 }
 
 int main()
 {
 	B.setOutPin (led);
-	
-	uart1.transmit(radio.startup);	
-	for (uint8_t i=0;i<5;++i)
+
+	if (radio.startup) lcd.string_screen (0,0,"STARTUP OK");
+	else lcd.string_screen (0,0,"STARTUP ERROR");
+	//lcd.bin_number (35,1,radio.r_reg(CONFIG));
+	/*for (uint8_t i=0;i<5;++i)
 	uart1.transmit (radio.r_reg(CONFIG));
 
 	uart1.transmit (radio.r_reg(EN_AA));
@@ -54,7 +54,7 @@ int main()
 		delay_ms (500);
 		uart1.transmit (radio.r_reg(FIFO_STATUS));
 		delay_ms (500);
-		uart1.transmit (radio.r_reg(FEATURE));
+		uart1.transmit (radio.r_reg(FEATURE));*/
 	/*
 	uart1.transmit("===");
 	for (uint8_t i=0;i<5;++i)
@@ -77,7 +77,7 @@ int main()
 	radio.rx_state ();*/
   while (1)
   {
-
-		
+	
   }
 }
+
